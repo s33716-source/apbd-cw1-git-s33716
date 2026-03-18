c718a8b758ab25b1421725cb9fc64823454b0d8b
Michlop <s33716@pjwstk.edu.pl>
2026-03-18 20:41:15 +0100

    Added new function Fib(uint n). It returns the nth fibonacci sequence number. Uses ulong to max out the size of output before overflowing.

--- gitTest/gitTest/MainClass.cs
+++ gitTest/gitTest/MainClass.cs
@@ -1,10 +1,30 @@
-﻿namespace gitTest
+﻿using System.Reflection.Emit;
+
+namespace gitTest
 {
     public class MainClass
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(Fib(10));
+        }
 
+        public static ulong Fib(uint n)
+        {
+            if(n == 0)
+                return 0;
+            if(n == 1)
+                return 1;
+
+            ulong i1 = 0;
+            ulong i2 = 1;
+            for(int i = 2; i <= n; i++)
+            {
+                ulong tmp = i1 + i2;
+                i1 = i2;
+                i2 = tmp;
+            }
+            return i2;
         }
     }
 }
